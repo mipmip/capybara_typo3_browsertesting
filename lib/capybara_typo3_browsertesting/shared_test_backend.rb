7b5ecd933be165752ee083e6762ece4ff3dbd561
Pim Snel <pim@lingewoud.nl>
2020-01-29 14:10:22 +0100

    new 9.5 backend check

--- lib/capybara_typo3_browsertesting/shared_test_backend.rb
+++ lib/capybara_typo3_browsertesting/shared_test_backend.rb
@@ -5,14 +5,19 @@ module CapybaraTypo3Browsertesting
     def test_backend
       visit '/typo3'
 
+      #t3-username
       within("#typo3-login-form") do
-        fill_in 't3-username', :with => CapybaraTypo3Browsertesting.typo3_backend_login
+        fill_in 't3-username', :with => CapybaraTypo3Browsertesting.typo3_frontend_login
         fill_in 't3-password', :with => CapybaraTypo3Browsertesting.typo3_backend_password
       end
       click_button 't3-login-submit'
 
-      # 7.6
-      assert page.has_selector?('#typo3-cms-backend-backend-toolbaritems-livesearchtoolbaritem')
+      if CapybaraTypo3Browsertesting.typo3_major_version == '7.6'
+        assert page.has_selector?('#typo3-cms-backend-backend-toolbaritems-livesearchtoolbaritem')
+
+      elsif CapybaraTypo3Browsertesting.typo3_major_version == '9.5'
+        assert page.has_selector?('#t3js-modal-backendlocked')
+      end
     end
 
   end
